3057bb2c6fc971a0c0412973d813b18d8e72002b
naezith <tolgaay@live.com>
2018-08-12 21:36:40 +0300

    Added rank images

--- src/components/LeaderboardLine.js
+++ src/components/LeaderboardLine.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
 
+let max_score = 10000;
+let ranks = [
+    'Novice',
+    'Apprentice',
+    'Adept',
+    'Veteran',
+    'Expert',
+    'Master',
+    'Grandmaster',
+    'Inhuman',
+    'Descendant'
+];
+
+let rank_images = '/img/ranks/';
+
 class LeaderboardLine extends Component {
+    getRankImage = (name) => ( <img src={rank_images + name + '.png'} alt={name} /> )
+    
+
     render() {
         let { badge, eq_rank, global_score, username } = this.props; // id, rank
-        let max_score = 10000;
-        let ranks = [
-            'Novice',
-            'Apprentice',
-            'Adept',
-            'Veteran',
-            'Expert',
-            'Master',
-            'Grandmaster',
-            'Inhuman',
-            'Descendant'
-        ];
 
         return (
             <tr>
                 <td>{ eq_rank }</td>
-                <td>{ ranks[badge] }</td>
+                <td>{ this.getRankImage(ranks[badge]) }</td>
                 <td>{ username }</td>
                 <td>{ parseFloat(100*global_score/max_score).toFixed(3) }%</td>
             </tr>
