2d0ff8fe87df9583204d0eb15206b2ded2314c25
naezith <tolgaay@live.com>
2018-08-16 11:25:02 +0300

    Removed constructor from Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,6 @@ import '../css/Header.css';
 import { Link } from 'react-router-dom'
 
 class Header extends Component {
-  constructor() {
-    super();
-    
-    this.state = {}; 
-  }
-  
   render() {
     return (
       <div className="header-div">
