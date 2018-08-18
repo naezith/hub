c7c550696dd270b3d3e36f585903d0abbda70c72
naezith <tolgaay@live.com>
2018-08-18 20:46:39 +0300

    Removed the undefined check inside component

--- src/components/render/DateText.jsx
+++ src/components/render/DateText.jsx
@@ -3,4 +3,4 @@ import React from 'react'
 import { prettifyDate, formatDate  } from '../../utility/formatters'
 
 export const DateText = ({date}) => 
-    date ? ( <span title={prettifyDate(date)}>{formatDate(date)}</span> ) : undefined
+    <span title={prettifyDate(date)}>{formatDate(date)}</span>
