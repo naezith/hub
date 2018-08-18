dd9424102f3e69544bbfe8d1a814e1d666a00c66
naezith <tolgaay@live.com>
2018-08-18 19:08:26 +0300

    World Records page now displays dates

--- src/components/render/WorldRecordLine.jsx
+++ src/components/render/WorldRecordLine.jsx
@@ -5,15 +5,16 @@ import { PlayerLink } from './PlayerLink';
 import { LevelLink } from './LevelLink';
 
 import { calcDominance } from '../../utility/calculations'
-import { formatDominance, formatTime } from '../../utility/formatters'
+import { formatDominance, formatTime, formatDate } from '../../utility/formatters'
 import { ranks } from '../../data/naezith'
 
-export const WorldRecordLine = ({ level_id, player_id, badge, username, global_score, time }) => 
+export const WorldRecordLine = ({ level_id, player_id, badge, username, global_score, time, update_date }) => 
     <tr>
         <td>{<LevelLink id={level_id}/>}</td>
         <td>{ formatTime(time) }</td>
         <td><RankIcon name={ranks[badge]} /></td>
         <td><PlayerLink id={player_id} username={username}/></td>
         <td>{ formatDominance(calcDominance(global_score, 'global')) }</td>
+        { update_date ? <th>{formatDate(update_date)}</th> : undefined }
     </tr>
 
