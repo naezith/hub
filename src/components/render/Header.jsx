af10f049c623ec72237dae60a6732a667479e8fa
naezith <tolgaay@live.com>
2018-08-21 00:55:19 +0300

    NavBar shows the current page

--- src/components/render/Header.jsx
+++ src/components/render/Header.jsx
@@ -3,16 +3,19 @@ import { Link } from 'react-router-dom'
 
 import { SteamLogin } from './SteamLogin'
 
+const navSelectedPage = path => path === window.location.pathname ? 'nav-selected' : 'nav-not-selected' 
+
 export const Header = ({ user }) => 
     <header>
       <div className='header-div'>
         <img className='header-icon' src={require('../../img/naezith-icon.png')} title={'naezith'} alt={'naezith'} />
         <h2 className='title'>hub.naezith</h2>
         <ul className='nav'>
-          <li><Link className='header-menu-link' to='/global-rankings'>Global Rankings</Link></li>
-          <li><Link className='header-menu-link' to='/world-records'>World Records</Link></li>
-          <li><Link className='header-menu-link' to='/players'>Search Players</Link></li>
+          <li><Link className={'header-menu-link' + ' ' + navSelectedPage('/global-rankings')} to='/global-rankings'>Global Rankings</Link></li>
+          <li><Link className={'header-menu-link' + ' ' + navSelectedPage('/world-records')} to='/world-records'>World Records</Link></li>
+          <li><Link className={'header-menu-link' + ' ' + navSelectedPage('/players')} to='/players'>Search Players</Link></li>
         </ul>
         <SteamLogin user={user}/>
       </div>
     </header>
+    
