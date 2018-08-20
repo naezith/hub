6d723e0fd79af3b45077d3891435c3e25423182f
naezith <tolgaay@live.com>
2018-08-20 23:01:29 +0300

    CSS: Header is static now

--- src/components/render/Header.jsx
+++ src/components/render/Header.jsx
@@ -5,12 +5,14 @@ import { SteamLogin } from './SteamLogin'
 
 export const Header = ({ user }) => 
     <header>
-      <img className='header-icon' src={require('../../img/naezith-icon.png')} title={'naezith'} alt={'naezith'} />
-      <h2 className='title'>hub.naezith</h2>
-      <ul className='nav'>
-        <li><Link className='header-menu-link' to='/global-rankings'>Global Rankings</Link></li>
-        <li><Link className='header-menu-link' to='/world-records'>World Records</Link></li>
-        <li><Link className='header-menu-link' to='/players'>Search Players</Link></li>
-      </ul>
-      <SteamLogin user={user}/>
+      <div className='header-div'>
+        <img className='header-icon' src={require('../../img/naezith-icon.png')} title={'naezith'} alt={'naezith'} />
+        <h2 className='title'>hub.naezith</h2>
+        <ul className='nav'>
+          <li><Link className='header-menu-link' to='/global-rankings'>Global Rankings</Link></li>
+          <li><Link className='header-menu-link' to='/world-records'>World Records</Link></li>
+          <li><Link className='header-menu-link' to='/players'>Search Players</Link></li>
+        </ul>
+        <SteamLogin user={user}/>
+      </div>
     </header>
