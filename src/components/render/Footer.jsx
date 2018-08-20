bb5eb8d70cfabac6afbcafe5ef149c1ad60c3ff3
naezith <tolgaay@live.com>
2018-08-21 01:02:30 +0300

    Footer links are same as nav ones

--- src/components/render/Footer.jsx
+++ src/components/render/Footer.jsx
@@ -4,6 +4,10 @@ export const Footer = () =>
     <footer>
         <div className='footer-div'>
             <p className='footer-copyright'>&copy; Tolga Ay 2018</p>
-            <p className='footer-links'><a href='https://naezith.com' target='_blank' rel='noopener noreferrer'>Remnants of Naezith</a> | <a href='https://discord.gg/naezith' target='_blank' rel='noopener noreferrer'>Discord Server</a> | <a href='https://github.com/naezith/ron-hub' target='_blank' rel='noopener noreferrer'>Source on GitHub</a></p>
+            <ul className='footer-links'>
+                <li><a href='https://naezith.com' target='_blank' rel='noopener noreferrer'>Remnants of Naezith</a></li>
+                <li><a href='https://discord.gg/naezith' target='_blank' rel='noopener noreferrer'>Discord Server</a></li>
+                <li><a href='https://github.com/naezith/ron-hub' target='_blank' rel='noopener noreferrer'>Source on GitHub</a></li>
+            </ul>
         </div>
     </footer>
--- src/components/render/Header.jsx
+++ src/components/render/Header.jsx
@@ -11,9 +11,9 @@ export const Header = ({ user }) =>
         <img className='header-icon' src={require('../../img/naezith-icon.png')} title={'naezith'} alt={'naezith'} />
         <h2 className='title'>hub.naezith</h2>
         <ul className='nav'>
-          <li><Link className={'header-menu-link' + ' ' + navSelectedPage('/global-rankings')} to='/global-rankings'>Global Rankings</Link></li>
-          <li><Link className={'header-menu-link' + ' ' + navSelectedPage('/world-records')} to='/world-records'>World Records</Link></li>
-          <li><Link className={'header-menu-link' + ' ' + navSelectedPage('/players')} to='/players'>Search Players</Link></li>
+          <li><Link className={navSelectedPage('/global-rankings')} to='/global-rankings'>Global Rankings</Link></li>
+          <li><Link className={navSelectedPage('/world-records')} to='/world-records'>World Records</Link></li>
+          <li><Link className={navSelectedPage('/players')} to='/players'>Search Players</Link></li>
         </ul>
         <SteamLogin user={user}/>
       </div>
