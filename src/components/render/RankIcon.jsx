6ce297e7058aba7692dca1a2d988c18f710ff8c5
naezith <tolgaay@live.com>
2018-08-21 01:48:10 +0300

    Added tooltip to Rank Icon

--- src/components/render/RankIcon.jsx
+++ src/components/render/RankIcon.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
 
 export const RankIcon = ({name}) => 
-    ( <img className='rank-icon' src={require('../../img/ranks/' + name + '.png')} title={name} alt={name} /> )
+    <span className='tooltip'>
+        <img className='rank-icon' src={require('../../img/ranks/' + name + '.png')} alt={name} /> 
+        <span className='tooltip-text'>{name}</span>
+    </span>
