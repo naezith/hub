3d1c52955b7f0bbcd1846e8ec6f03ae9c4c4d982
naezith <tolgaay@live.com>
2018-08-20 22:20:46 +0300

    Added thead and tbody back

--- src/components/render/Leaderboard.jsx
+++ src/components/render/Leaderboard.jsx
@@ -15,33 +15,36 @@ export const Leaderboard = ({ start_rank, lines, loading, changePage,
             <button onClick={(event) => changePage(event, 'next')} disabled={loading}>Next</button> 
             </div> : undefined}
         <table className={lines[0].steam_id === undefined ? 'levels' : 'players'}>
-            <tr>
-                { lines[0].rank === undefined ? undefined : 
-                    <th>Rank</th> }
-                { lines[0].level_id === undefined ? undefined : 
-                    <th>Level</th> }
-                { lines[0].player_id === undefined ? undefined : 
-                    <th>Player</th> }
-                { lines[0].score === undefined ? undefined : 
-                    <th>Dominance</th> }
-                { lines[0].time === undefined ? undefined : 
-                    <th>Time</th> }
-                { lines[0].official_time === undefined ? undefined : 
-                    <th>Mastery Time</th> }
-                { lines[0].lb_rank === undefined ? undefined : 
-                    <th>Rank</th> }
-                { lines[0].update_date === undefined ? undefined : 
-                    <th>{date_header}</th> }
-                { extra_header === undefined ? undefined : 
-                    <th>{extra_header}</th> }
-                { lines[0].steam_id === undefined ? undefined : 
-                    <th></th> }
-            </tr>
-
+            <thead>
+                <tr>
+                    { lines[0].rank === undefined ? undefined : 
+                        <th>Rank</th> }
+                    { lines[0].level_id === undefined ? undefined : 
+                        <th>Level</th> }
+                    { lines[0].player_id === undefined ? undefined : 
+                        <th>Player</th> }
+                    { lines[0].score === undefined ? undefined : 
+                        <th>Dominance</th> }
+                    { lines[0].time === undefined ? undefined : 
+                        <th>Time</th> }
+                    { lines[0].official_time === undefined ? undefined : 
+                        <th>Mastery Time</th> }
+                    { lines[0].lb_rank === undefined ? undefined : 
+                        <th>Rank</th> }
+                    { lines[0].update_date === undefined ? undefined : 
+                        <th>{date_header}</th> }
+                    { extra_header === undefined ? undefined : 
+                        <th>{extra_header}</th> }
+                    { lines[0].steam_id === undefined ? undefined : 
+                        <th></th> }
+                </tr>
+            </thead>
+            <tbody>
             { lines.map((l, i) => <LeaderboardLine key={i} {...l} 
                                     dominance_scale={dominance_scale} 
                                     dominance_precision={dominance_precision} 
                                     obj={l} 
                                     extra_value_func={extra_value_func} />) }
+            </tbody>
         </table>
     </div>
