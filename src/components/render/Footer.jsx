b3b2ca84d0d4c6dd97b4c3303d77dcb18d4647a1
naezith <tolgaay@live.com>
2018-08-20 23:23:42 +0300

    CSS: Fixed footer

--- src/components/render/Footer.jsx
+++ src/components/render/Footer.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export const Footer = () => 
     <footer>
-        <p className='footer-copyright'>&copy; Tolga Ay 2018</p>
-        <p className='footer-links'><a href='https://naezith.com' target='_blank' rel='noopener noreferrer'>Remnants of Naezith</a> | <a href='https://discord.gg/naezith' target='_blank' rel='noopener noreferrer'>Discord Server</a> | <a href='https://github.com/naezith/ron-hub' target='_blank' rel='noopener noreferrer'>Source on GitHub</a></p>
+        <div className='footer-div'>
+            <p className='footer-copyright'>&copy; Tolga Ay 2018</p>
+            <p className='footer-links'><a href='https://naezith.com' target='_blank' rel='noopener noreferrer'>Remnants of Naezith</a> | <a href='https://discord.gg/naezith' target='_blank' rel='noopener noreferrer'>Discord Server</a> | <a href='https://github.com/naezith/ron-hub' target='_blank' rel='noopener noreferrer'>Source on GitHub</a></p>
+        </div>
     </footer>
