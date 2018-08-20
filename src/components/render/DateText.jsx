1fdf4341cf2feb0b409eb1746a81b9d873caf378
naezith <tolgaay@live.com>
2018-08-21 01:34:19 +0300

    Added tooltip to dates to show the full date

--- src/components/render/DateText.jsx
+++ src/components/render/DateText.jsx
@@ -3,4 +3,7 @@ import React from 'react'
 import { prettifyDate, formatDate  } from '../../utility/formatters'
 
 export const DateText = ({date}) => 
-    <span title={prettifyDate(date)}>{formatDate(date)}</span>
+    <span className='tooltip'>
+        {formatDate(date)}
+        <span className='tooltip-text'>{prettifyDate(date)}</span>
+    </span>
