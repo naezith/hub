4d6ab0e1479d117d57cb8eaee5978a453f739be0
naezith <tolgaay@live.com>
2018-08-22 04:01:59 +0300

    Logout button polish

--- src/components/render/Footer.jsx
+++ src/components/render/Footer.jsx
@@ -6,7 +6,7 @@ export const Footer = () =>
     <footer>
         <p className='footer-copyright'>&copy; Tolga Ay 2018</p>
         <ul className='footer-links'>
-            <li><Link className='logout' to={'/steam/logout'}>Logout</Link></li>
+            <li className='logout'><Link to={'/steam/logout'}>Logout</Link></li>
             <li><a href='https://github.com/naezith/hub' target='_blank' rel='noopener noreferrer'>Source on GitHub</a></li>
             <li><a href='https://discord.gg/naezith' target='_blank' rel='noopener noreferrer'>Discord Server</a></li>
             <li><a href='https://naezith.com' target='_blank' rel='noopener noreferrer'>Remnants of Naezith</a></li>
