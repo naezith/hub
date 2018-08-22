1af81f8f5f646d90cf197974a058ba80e85f0cd2
naezith <tolgaay@live.com>
2018-08-22 03:49:37 +0300

    Cleanup

--- src/components/render/SteamLogin.jsx
+++ src/components/render/SteamLogin.jsx
@@ -7,25 +7,29 @@ import { navSelectedPage } from '../../utility/common'
 
 export const SteamLogin = ({user}) => 
     user === undefined ?  
-        <ul className='steam'>
-            <li className='nav-profile'><Link to={'/steam'}><img src='/img/steam/sits_01.png' alt='Login with Steam'/></Link></li>
-        </ul>    
+        <Link className='nav-profile' to={'/steam'}><img src='/img/steam/sits_01.png' alt='Login with Steam'/></Link>
             :
-        <ul className='steam'>
-            <li className='nav-profile'>{<PlayerLink navSelected={navSelectedPage('/player/' + user.steamid)} 
-                            id={user.steamid} username={user.personaname} steam_info={user} />}</li>
-        </ul>
+        <PlayerLink navSelected={'nav-profile ' + navSelectedPage('/player/' + user.steamid)} 
+                            id={user.steamid} username={user.personaname} steam_info={user} />
         
 /*
-avatar:'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/fe/fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb.jpg'
-avatarfull:'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/fe/fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb_full.jpg'
-avatarmedium:'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/fe/fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb_medium.jpg'
-commentpermission:'2'
-communityvisibilitystate:'1'
-lastlogoff:'1521973305'
-personaname:'nae'
-personastate:'0'
-profilestate:'1'
-profileurl:'https://steamcommunity.com/profiles/76561198796210443/'
-steamid:'76561198796210443'
+{
+      "avatar": "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/3c/3c053319176594760e26c99ad516276e6b694e2a.jpg",
+      "avatarfull": "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/3c/3c053319176594760e26c99ad516276e6b694e2a_full.jpg",
+      "avatarmedium": "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/3c/3c053319176594760e26c99ad516276e6b694e2a_medium.jpg",
+      "commentpermission": "1",
+      "communityvisibilitystate": "3",
+      "lastlogoff": "1534853595",
+      "loccountrycode": "TR",
+      "locstatecode": "34",
+      "personaname": "naezith",
+      "personastate": "0",
+      "personastateflags": "0",
+      "primaryclanid": "103582791456604229",
+      "profilestate": "1",
+      "profileurl": "https://steamcommunity.com/id/naezith/",
+      "realname": "Tolga",
+      "steamid": "76561198013519231",
+      "timecreated": "1252497008"
+    }
 */
