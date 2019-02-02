d74ca15eeaf74f8a7df210818d0c12ff99dcd540
naezith <tolgaay@live.com>
2019-02-03 00:41:23 +0300

    Changed proxy

--- src/utility/steamapi.js
+++ src/utility/steamapi.js
@@ -1,7 +1,7 @@
 import { steam } from '../secrets'
 import { fetchDataGET } from './common'
 
-const proxyUrl = 'https://cors-proxy.htmldriven.com/?url='
+const proxyUrl = 'https://cors.io/?'
 
 const getCountryIconURL = code =>
     code && ('https://steamcommunity-a.akamaihd.net/public/images/countryflags/' + (code).toLowerCase() + '.gif')
