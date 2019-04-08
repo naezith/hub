0c42ee23107f9c5b9e3c930c99f973ff7f96140c
naezith <naezith@users.noreply.github.com>
2019-04-08 13:53:52 +0300

    Adding Level of the Day

--- src/components/render/pages/WorldRecords.jsx
+++ src/components/render/pages/WorldRecords.jsx
@@ -3,9 +3,18 @@ import React from 'react'
 import { Leaderboard } from '../Leaderboard'
 import { Loading } from '../Loading'
 
-export const WorldRecords = ({ levels, most_wrs, loading }) => 
+export const WorldRecords = ({ levels, most_wrs, loading }) => {
+    const now = new Date();
+    const start = new Date(now.getFullYear(), 0, 0);
+    const diff = now - start;
+    const oneDay = 1000 * 60 * 60 * 24;
+    const day = Math.floor(diff / oneDay);
+
+    const level_of_the_day = levels[day % levels.length]
+
+    return (
         loading > 0 ?  <Loading /> : 
-        
+
         <div>
             <h2>Record Holders</h2>
             <Leaderboard lines={most_wrs} 
@@ -18,4 +27,8 @@ export const WorldRecords = ({ levels, most_wrs, loading }) =>
             <h2>Levels</h2>
             <Leaderboard lines={levels} 
                          loading={loading} />
+
+           <h4>Level of the Day: {levels.name}</h4> 
         </div>
+    )
+}
