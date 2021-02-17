4900d0fa83a5854a994e26b762dcb6759a02a25f
naezith <naezith@users.noreply.github.com>
2021-02-17 13:25:59 +0300

    better url

--- src/utility/steamapi.js
+++ src/utility/steamapi.js
@@ -12,8 +12,7 @@ const getSteamInfo = steam_ids => {
         steam_ids.forEach(id => id_list += id + ',')
         id_list = id_list.substr(0, id_list.length - 1)
 
-        fetchDataGET(proxyUrl + 'https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=' +
-                                steam.key + '&steamids=' + id_list)().then(content => {
+        fetchDataGET(proxyUrl + 'https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?steamids=' + id_list + '&key=' + steam.key)().then(content => {
             if(!content.error_msg) {
                 content.response.players.forEach(p => p.country_icon = getCountryIconURL(p.loccountrycode))
 
