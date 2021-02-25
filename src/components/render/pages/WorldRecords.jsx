6ec8b1023cc6cf97c2cbb211cb3f7f9ed79a9075
naezith <naezith@gmail.com>
2021-02-25 12:24:21 +0300

    add sum of WRs

--- src/components/render/pages/WorldRecords.jsx
+++ src/components/render/pages/WorldRecords.jsx
@@ -4,6 +4,7 @@ import { Leaderboard } from '../Leaderboard'
 import { Loading } from '../Loading'
 import { getChapterName } from '../../../utility/ron-hub';
 import shuffleSeed from 'shuffle-seed'
+import { formatTime } from '../../../utility/formatters'
 
 export const WorldRecords = ({ levels, speedruns, most_wrs, loading }) => {
     // Filter out the secret levels
@@ -16,11 +17,18 @@ export const WorldRecords = ({ levels, speedruns, most_wrs, loading }) => {
     
     // Shuffle levels and pick the level of the day
     const level_of_the_day = shuffleSeed.shuffle(non_secret_levels, seed)[daysSinceEpoch % non_secret_levels.length]
+    
+    // Sum of WRs
+    let sum_of_wrs = 0
+    levels.map(l => sum_of_wrs += l.time)
 
     return (
         loading > 0 ?  <Loading /> : 
 
         <div>
+            <hr/>
+            <h2>Sum of WRs {formatTime(sum_of_wrs)}</h2>
+            <hr/>
             <h2>Record Holders</h2>
             <Leaderboard lines={most_wrs} 
                          loading={loading} 
